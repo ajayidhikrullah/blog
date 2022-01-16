cdf0ccf9b5f509e4528dbd06407a5e45125cc479
ajayidhikrullah <ajayidhikrullah@gmail.com>
2022-01-16 17:17:38 +0000

    created the body and title page to show the Blog post and body + created at and created by

--- index.php
+++ index.php
@@ -1,12 +1,49 @@
-
-
-	<?php
+<?php
 	include 'includes/header.php';
-	include 'includes/signup.inc.php';
-	$firstname = $_SESSION['firstName'];
-	echo "<p style='color:Green;'> Welcome, you have successfully registedred to our blog post @ <b>" . $firstname . "</b> </p>";
 	
+?>
+
+<div class="container">
+	<?php
+		include 'includes/signup.inc.php';
+		$firstname = $_SESSION['firstName'];
+		echo "<p style='color:Green;'> Welcome, you have successfully registedred to our blog post @ <b>" . $firstname . "</b> </p>";
 	?>
+
+	<div class="jumbotron">
+	<h1 class="display-5">Top-rated blog title goes here...</h1>
+	<p class="lead">Body goes here...</p>
+	<hr class="my-4">
+	<p>Created By: Dhikr</p>
+	<p>Created at: Nurudeen</p>
+	<p class="lead">
+		<a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
+	</p>
+	</div>
+
+	<div class="row">
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Special topics</h5>
+        <p class="card-text">Body goes in here...</p>
+        <a href="#" class="btn btn-primary">Click to read more...</a>
+      </div>
+    </div>
+  </div>
+
+  <div class="col-sm-6">
+    <div class="card">
+      <div class="card-body">
+        <h5 class="card-title">Title also goes here...</h5>
+        <p class="card-text">Body goes here...</p>
+        <a href="#" class="btn btn-primary">Click to read more</a>
+      </div>
+    </div>
+  </div>
+</div>
+
+</div>
 
 
 
