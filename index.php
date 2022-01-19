7587390b17706885ee03df4d9000610a41f6af03
ajayidhikrullah <ajayidhikrullah@gmail.com>
2022-01-19 00:59:16 +0000

    returned each row in post table to users using foreach

--- index.php
+++ index.php
@@ -19,40 +19,50 @@
 		<?php 
 		echo "<h1 class='display-5'> " . $post[0]['title'] . "</h1>" .
 	// }
-// fetch post body here
-	"<p class='lead'>" . $post[1]['body'] . "</p>";
+	// fetch post body here
+	"<p class='lead'>" . $post[0]['body'] . "</p>" . 
 
-
+	"<p><b> Created at:</b>" . $post[0]['created_at'] . "</p>"
+	// fetch the date the post was created
 ?>
 
 	<hr class="my-4">
 	<p>Author: Dhikr</p>
-	<p>Created at: Nurudeen</p>
+	<!-- <p>Created at: 00:01:2020</p> -->
 	<p class="lead">
 		<a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
 	</p>
 	</div>
+	<!-- echo "
+		<div class='row'>
+			<div class='col-sm-6'>
+				<div class='card border-success mb-3'>
+				<div class='card-body'>
+					<h5 class='card-title'>" . $post[1]['title'] . "</h5>
+					<p class='card-text'>" . $post[1]['body'] . "</p>
+					<a href='#' class='btn btn-primary'> Click to read more...</a>
+				</div>
+				</div>
+			</div>"; -->
+	
 
-	<div class="row">
-		<div class="col-sm-6">
-			<div class="card border-success mb-3">
-			<div class="card-body">
-				<h5 class="card-title">Special topics</h5>
-				<p class="card-text">Body goes in here...</p>
-				<a href="#" class="btn btn-primary">Click to read more...</a>
-			</div>
-			</div>
-		</div>
+		<?php foreach($post as $eachRow){ 
+		?>
+			<?php echo 
+			// var_dump($eachRow);exit();
 
-		<div class="col-sm-6">
-			<div class="card border-danger">
-			<div class="card-body">
-				<h5 class="card-title">Title also goes here...</h5>
-				<p class="card-text">Body goes here...</p>
-				<a href="#" class="btn btn-primary">Click to read more</a>
-			</div>
-			</div>
-		</div>
+				"<div class='col-sm-6'>
+					<div class='card border-danger'>
+					<div class='card-body'>
+						<h5 class='card-title'>". $eachRow['title'] ."</h5>
+						<p class='card-text'>" . $eachRow['body'] . "</p>
+						<a href='#' class='btn btn-primary'>Click to read more</a>
+					</div>
+					</div>
+				</div>";?>
+			<?php
+				};
+			?>
 </div>
 
 
