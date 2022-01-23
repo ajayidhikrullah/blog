878f6b21d2fab01a425799dace64fd338a208545
ajayidhikrullah <ajayidhikrullah@gmail.com>
2022-01-23 13:50:58 +0000

    added the hamburger to handle the navbars when they are minimized, done with the header

--- includes/header.php
+++ includes/header.php
@@ -54,11 +54,11 @@
 
 </header>
 
+<!-- jquery -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
-
-
-
-
+<!-- custom script -->
+<script src="js/script.js"></script>
 
 <!-- ============================================= -->
 <!-- 
---
+++ js/script.js
@@ -0,0 +1,8 @@
+// alert('js connected');
+
+$(document).ready(function(){
+    $('.menu-toggle').on('click', function(){
+        $('.nav').toggleClass('showing');
+        $('.nav ul').toggleClass('showing');
+    });
+});
