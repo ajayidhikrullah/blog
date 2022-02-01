9e94b5094eab5267cc2991e22eaa8b95715186e7
ajayidhikrullah <ajayidhikrullah@gmail.com>
2022-02-01 00:38:56 +0000

    created the footer sections

--- index.php
+++ index.php
@@ -114,65 +114,12 @@
 <div class="footer">
 	<div class="footer-content">
 		<div class="footer-section section"></div>
-		<div class="footer-section links"></div>
 		<div class="footer-section contact-form"></div>
 	</div>
 
 	<div class="footer-bottom">
 		&copy; ajayisikiru.com | Designed by Dhikrullah
-
 	</div>
-</div>
-
-<!-- OLD -->
-<div class="container">
-
-
-	<div class="jumbotron">
-<?php
-	// var_dump($post[0]['title']);exit();
-	// foreach ($post as $eachPost){?>
-		<?php 
-		echo "<center><h1 class='display-5'> " . $post[0]['title'] . "</h1>" .
-	// }
-	// fetch post body here
-	"<center><p class='lead'>" . $post[0]['body'] . "</p>" . 
-
-	"<center><p><b> Created at:</b>" . $post[0]['created_at'] . "</p></center>"
-	// fetch the date the post was created
-?>
-
-	<hr class="my-4">
-	<center><p>Author: Dhikr</p>
-	<!-- <p>Created at: 00:01:2020</p> -->
-	<p class="lead">
-		<a class="btn btn-primary btn-lg" href="#" role="button">Learn more</a>
-	</p></center>
-	</div>
-	
-		<div class='row'>
-		<?php foreach($post as $eachRow){ 
-		?>
-			<?php echo 
-			// var_dump($eachRow);exit();
-
-				"<div class='col-sm-6'>
-					<div class='card border-success'>
-					<div class='card-body'>
-						<h5 class='card-title'>". $eachRow['title'] ."</h5>
-						<p class='card-text'>" . $eachRow['body'] . "</p>
-						<a href='#' class='btn btn-primary'>Click to read more</a>
-					</div>
-					</div>
-				</div>";?>
-			<?php
-				};
-			?>
-</div>
-
-<?php
-	include 'includes/footer.php';
-?>
 </div>
 
 
