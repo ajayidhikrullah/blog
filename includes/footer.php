e11ac1d626c896891fad9d5910ea00a47eb88a7f
ajayidhikrullah <ajayidhikrullah@gmail.com>
2022-02-06 01:43:33 +0000

    added the social media links

--- includes/footer.php
+++ includes/footer.php
@@ -4,8 +4,20 @@
 <div class="footer">
 	<div class="footer-content">
 		<div class="footer-section about">
-      
          <h1 class="logo-text"><span>Dhikrullah</span>Blog</h1>
+         <p>Just another blog site built with HTML, CSS, JS and PHP</p>
+
+         <div class="contact">
+            <span><i class="fa fa-phone"></i>&nbsp; +44-077-3277-4822</span>
+            <span><i class="fas fa-envelope"></i> &nbsp; ajayidhikrullah@gmail.com</span>
+
+            <div class="socials">
+               <a href="#"><i class="fab fa-linkedin"></i></i></a>
+               <a href="#"><i class="fab fa-twitter"></i></a>
+               <a href="#"><i class="fab fa-github"></i></a>
+            </div>
+
+         </div>
       </div>
 		
       
