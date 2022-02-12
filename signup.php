9cc0d6a6a38249802621cd444860b31498d32728
ajayidhikrullah <ajayidhikrullah@gmail.com>
2022-02-12 09:40:05 +0000

    redesigned te signup page

--- signup.php
+++ signup.php
@@ -3,27 +3,25 @@
 ?>
 
 <body class="form-v5">
-	<div class="page-content">
-		<div class="form-v5-content">
-
+		<div class="auth-content">
 			<form class="form-detail" method="post" action="includes/signup.inc.php">
-				<h2>Signup to  register your blog account for free</h2>
-				<div class="form-row">
+				<h2 class="form-title">Signup to  register your blog account for free</h2>
+				<div class="">
 					<label for="firstName">First Name</label>
-					<input type="text" name="firstName" id="firstName" class="input-text" placeholder="Your first name please" required>
+					<input type="text" name="firstName" id="firstName" class="text-input" placeholder="Your first name please" required>
 					<i class="fas fa-user"></i>
 				</div>
 
-                <div class="form-row">
+                <div class="">
 					<label for="lastName">Last Name</label>
-					<input type="text" name="lastName" id="lastName" class="input-text" placeholder="Your surname or lastname please" required>
+					<input type="text" name="lastName" id="lastName" class="text-input" placeholder="Your surname or lastname please" required>
 					<i class="fas fa-user"></i>
 				</div>
 
                 
-				<div class="form-row">
+				<div class="">
 					<label for="your-email">Your Email</label>
-					<input type="text" name="email" id="your-email" class="input-text" placeholder="Your Email" required pattern="[^@]+@[^@]+.[a-zA-Z]{2,6}">
+					<input type="text" name="email" id="your-email" class="text-input" placeholder="Your Email" required pattern="[^@]+@[^@]+.[a-zA-Z]{2,6}">
 					<i class="fas fa-envelope"></i>
 				</div>
 				<div class="form-row">
@@ -40,10 +38,13 @@
 
 
 				<div class="form-row-last">
-					<input type="submit" name="submit" class="register" value="SignUp">
+					<button type="submit" name="submit" class="btn btn-big">
+						SignUp
+					</button>
+					<!-- <input type="submit" name="submit" class="register" value="SignUp"> -->
 				</div>
+				<h5>Or <a href="login.php">Login here...</a></h5>
 			</form>
 		</div>
-	</div>
 </body>
 </html>
