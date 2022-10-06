6efa2cee871ca05c6a1e02128c255ee3eaa124ba
Ajayi <ajayidhikrullah@gmail.com>
2022-10-06 13:18:34 +0100

    added full pust view

---
+++ singlePost.php
@@ -0,0 +1,40 @@
+<?php
+    include 'includes/header.php';
+
+?>
+
+<!-- page wrapper -->
+<!-- carousel -->
+
+<div class="page-wrapper">
+	<!-- Body contents -->
+	<div class="content clearfix">
+		<div class="main-content singlepost">
+			<h1 class="post-title">Title of our Post</h1>
+		</div>
+            <!-- sidebar -->
+			<div class="sidebar singlesidebar">
+				
+				<div class="section topics">
+					<h2 class="section-title">Subject</h2>
+					<ul>
+						<li><a href="#">Poems</a></li>
+						<li><a href="#">Quotes</a></li>
+						<li><a href="#">Business</a></li>
+						<li><a href="#">Politics</a></li>
+					</ul>
+				</div>
+			</div>
+	</div>
+	<!-- content end -->
+
+</div>
+
+
+<!-- footer goes here -->
+<?php
+	include 'includes/footer.php';
+?>
+</body>
+</html>
+
