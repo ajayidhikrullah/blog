630ec76d5e55cfaa98f9380df3c626bb88eed4be
Ajayi <ajayidhikrullah@gmail.com>
2022-10-15 12:12:50 +0100

    added create post, users, topics and admin styles

---
+++ admin/posts/postIndex.php
@@ -0,0 +1,75 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>DeeBlog</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<link rel="stylesheet" type="text/css" href="css/roboto-font.css">
+<link rel="stylesheet" type="text/css" href="fonts/font-awesome-5/css/fontawesome-all.min.css">
+<link rel="stylesheet" href="../../css/style.css">
+<link rel="stylesheet" href="../../css/admin.css">
+
+<title>Admin || Manage Posts</title>
+</head>
+
+<body>
+<header>
+  <div class="logo">
+    <h1 class="logo-text"><span>Dhikrullah</span>Blog</h1>
+  </div>
+  <!-- burger app bar -->
+  <i class="fa fa-bars menu-toggle"></i>
+
+  <!-- Nav bar -->
+
+  <ul class="nav">
+    
+    <li>
+      <a class="" id="" href="#">
+        <i class="fa fa-user"></i>      
+          Dhikrullah
+        <i class="fa fa-chevron-down" style="font-size: .8em;"></i>      
+      </a>
+      <ul>
+        <li><a class="logout" id="" href="#">Logout</a></li>
+      </ul>
+    </li>
+  </ul>
+
+</header>
+
+
+
+
+<!-- jquery -->
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+<!-- CDN of carousel slick that I used -->
+<script src="https://cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+<!-- custom script -->
+<script src="js/script.js"></script>
+
+<!-- ============================================= -->
+<!-- 
+
+<div class="container mt-3">
+<ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+  <li class="nav-item">
+    <a class="nav-link active" id="pills-home-tab" data-toggle="pill" href="index.php" role="tab" aria-controls="pills-home" aria-selected="true">Home</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" id="pills-profile-tab" data-toggle="pill" href="signup.php" role="tab" aria-controls="pills-profile" aria-selected="false">Signup</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" id="pills-contact-tab" data-toggle="pill" href="login.php" role="tab" aria-controls="pills-contact" aria-selected="false">Login</a>
+  </li>
+
+  <li class="nav-item">
+    <a class="nav-link" id="pills-contact-tab" data-toggle="pill" href="post.php" role="tab" aria-controls="pills-contact" aria-selected="false">Create Post</a>
+  </li>
+</ul>
+
+</div> -->
+</body>
+</html>
